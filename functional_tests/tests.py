72518ea5e7bb8e6e0e00ac9b5f320dcc6a210685
Wayne <waynezhu119@hotmail.com>
2017-06-18 22:49:45 -0400

    new URL + view for adding to existing lists. FT passes :-)

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -43,6 +43,8 @@ class NewVisitorTest(LiveServerTestCase):
                         'Enter a to-do item'
         )
 
+        edith_list_url = self.browser.current_url
+
         # 她在一个文本框中输入了“Buy peacock feathers(”购买孔雀羽毛)
         # 伊迪丝的爱好是使用假蝇做鱼饵钓鱼
         inputbox.send_keys('Buy peacock feathers')
@@ -63,14 +65,36 @@ class NewVisitorTest(LiveServerTestCase):
 
         # 页面再次更新,清单中显示了这两个待办事项
         with self.wait_for_page_load(timeout=10):
-            self.check_for_row_in_list_table('1: Buy peacock feathers')
             self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+            self.check_for_row_in_list_table('1: Buy peacock feathers')
 
-        self.fail('Finish the test!')
+        # 现在一个叫作弗朗西斯的新用户访问了网站
+        ## 我们使用一个新浏览器会话
+        ## 确保伊迪丝的信息不会从cookie中泄露出来 #➊
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(5)
 
+        # 弗朗西斯访问首页
+        # 页面中看不到伊迪丝的清单
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        #self.assertNotIn('Buy peacock feathers', page_text)
+        #self.assertNotIn('make a fly', page_text)
 
-        # 伊迪丝想知道这个网站是否会记住她的清单
-        # 她看到网站为她生成了一个唯一的URL
-        # 而且页面中有一些文字解说这个功能
-        # 她访问那个URL,发现她的待办事项列表还在
-        # 她很满意,去睡觉了
+        # 弗朗西斯输入一个新待办事项,新建一个清单
+        # 他不像伊迪丝那样兴趣盎然
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+
+        # 弗朗西斯获得了他的唯一URL
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, '/lists/.+')
+        self.assertNotEqual(francis_list_url, edith_list_url)
+
+        # 这个页面还是没有伊迪丝的清单
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertIn('Buy milk', page_text)
+        # 两人都很满意,去睡觉了
