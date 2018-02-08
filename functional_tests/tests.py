b0a0fb9d1057a8c34936def85b482b6a8ad0d770
Wayne Zhu <waynezhu119@outlook.com>
2018-02-07 23:07:53 -0500

    Hack FT

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -4,8 +4,23 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support.expected_conditions import staleness_of
 from contextlib import contextmanager
+import sys
 
 class NewVisitorTest(StaticLiveServerTestCase):
+
+    @classmethod
+    def setUpClass(cls): #➊
+       for arg in sys.argv: #➋
+         if 'liveserver' in arg: #➌
+             cls.server_url = 'http://' + arg.split('=')[1] #➍
+             return #➎
+       super().setUpClass()
+       cls.server_url = cls.live_server_url
+
+    @classmethod
+    def tearDownClass(cls):
+       if cls.server_url == cls.live_server_url:
+           super().tearDownClass()
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -29,7 +44,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         # 伊迪丝听说有一个很酷的在线待办事项应用
         # 她去看了这个应用的首页
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
 
         # 她注意到网页的标题和头部都包含“To-Do”这个词
         self.assertIn('To-Do', self.browser.title)
@@ -77,7 +92,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
         # 弗朗西斯访问首页
         # 页面中看不到伊迪丝的清单
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         page_text = self.browser.find_element_by_tag_name('body').text
         #self.assertNotIn('Buy peacock feathers', page_text)
         #self.assertNotIn('make a fly', page_text)
@@ -102,7 +117,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
     def test_layout_and_styling(self):
         # 伊迪丝访问首页
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         self.browser.set_window_size(1024, 768)
 
         # 她看到输入框完美地居中显示
